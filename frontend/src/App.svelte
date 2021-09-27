5f39bf62b95aa4d82893a44266a3d7ecb5e33e17
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-09-28 03:07:36 +0700

    Add Header

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,97 @@
 <script lang="ts">
-	export let name: string;
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Header from "./components/Header.svelte";
+
+  export let name: string;
 </script>
 
-<main lang="pug">
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<template>
+  <Header />
+  <Router>
+    <Route path="/" component="{Home}" />
+  </Router>
+</template>
 
-<style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style lang="scss" global>
+  @import url(https://fonts.googleapis.com/css2?family=Roboto&display=swap);
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  body {
+    margin: 0;
+    padding: 0;
+    font-family:
+      "Roboto",
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      sans-serif;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  code {
+    font-family: "source-code-pro", "Menlo", "Monaco", "Consolas", "Courier New", monospace;
+  }
+  
+  html {
+    @extend body;
+
+    box-sizing: border-box;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  button {
+    display: block;
+    width: 100%;
+    border: none;
+    font: inherit;
+    cursor: pointer;
+  }
+
+  input {
+    margin: 0;
+    background-color: transparent;
+    font: inherit;
+    
+    &:focus { outline: none; }
+    &::-webkit-inner-spin-button { -webkit-appearance: none; }
+  }
+
+  a {
+    text-decoration: none;
+
+    &:visited {
+      color: inherit;
+    }
+  }
+  
+  * {
+    box-sizing: inherit;
+
+    &:not(textarea),
+    &:not([contenteditable=true]),
+    &:not(input) {
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+      -moz-user-select: none; /* Firefox */
+      -ms-user-select: none; /* Internet Explorer/Edge */
+      user-select: none; /* supported by Chrome and Opera */
+    }
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
 </style>
---
+++ frontend/src/components/Header.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { Router, Link } from "svelte-routing";
+</script>
+
+<template>
+  <Router>
+    <header>
+      <aside>
+
+      </aside>
+      <aside id="aside-logo-container" style="background-image: url(images/logo.png)"></aside>
+      <aside id="right-container">
+        <Link to="/login">
+          <div>LOGIN</div>
+        </Link>
+      </aside>
+    </header>
+  </Router>
+</template>
+
+<style lang="scss">
+  header {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: 100%;
+    background-color: #A8A5A5;
+    height: 54px;
+    padding: 0.5em 2em;
+
+    > aside {
+      display: flex;
+      align-self: center;
+      justify-self: center;
+    }
+  }
+
+  #aside-logo-container {
+    height: 100%;
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+
+  #right-container {
+    justify-self: end;
+
+    div {
+      cursor: pointer;
+      text-decoration: underline;
+    }
+  }
+</style>
